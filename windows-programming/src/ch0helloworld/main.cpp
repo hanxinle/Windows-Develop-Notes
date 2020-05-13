1185e60fc814f6ebcd7dcb306a8c4c7553792237
hanxinle <hanxloop@foxmail.com>
2020-05-13 23:06:36 +0800

    dll

---
+++ windows-programming/src/ch0helloworld/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <windows.h>
+
+using namespace std;
+
+int main() {
+    MessageBox(NULL, "窗口显示问题", "标题", MB_OKCANCEL);
+    return 0;
+}
+
+// use X64 - no "const char * \ LPSWSTR" problem
---
+++ windows-programming/src/ch1dll_exports/myfunc.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "myfunc.h"
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+
+int Cal::subb(int a, int b) {
+    return a - b;
+}
