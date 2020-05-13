ba783b91885f15e403d105c67f224114b1410107
hanxinle <hanxloop@foxmail.com>
2020-05-13 23:06:36 +0800

    dll

---
+++ windows-programming/src/ch2_dll_test/myfunc.cpp
@@ -0,0 +1,13 @@
+﻿#include "myfunc.h"
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int myfunc::sub(int a, int b) {
+    return a - b;
+}
+
+int Cal::subb(int a, int b) {
+    return a - b;
+}
---
+++ windows-programming/src/ch2_dll_test/myfunc.h
@@ -0,0 +1,17 @@
+﻿#ifdef CH1DLLEXPORTS_EXPORTS
+#define CH1DLLEXPORTS_API __declspec(dllexport)
+#else
+#define CH1DLLEXPORTS_API __declspec(dllimport)
+#endif
+
+CH1DLLEXPORTS_API int sum(int a, int b);
+
+class CH1DLLEXPORTS_API myfunc {
+public:
+    int sub(int a, int b);
+};
+
+class CH1DLLEXPORTS_API Cal {
+public:
+    int subb(int a, int b);
+};
