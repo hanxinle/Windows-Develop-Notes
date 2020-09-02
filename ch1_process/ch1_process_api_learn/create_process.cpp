01be37fb8700e26ba4f6205a321b589d46b56e20
hanxinle <hanxloop@foxmail.com>
2020-09-03 01:03:14 +0800

    process

---
+++ ch1_process/ch1_process_api_learn/create_process.cpp
@@ -0,0 +1,47 @@
+﻿/* 
+* CreateProcess
+* CloseHandle
+* ExitProcess
+* TerminateProcess
+*/ 
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main1() {
+    //初始化 CreateProcess 参数
+    char szCommandLine[] = "notepad.exe";
+    STARTUPINFO si;
+    memset(&si, 0, sizeof(si));
+    PROCESS_INFORMATION pi;
+
+    si.dwFlags = STARTF_USESHOWWINDOW;
+    si.wShowWindow = TRUE;
+
+    bool bRet = ::CreateProcess(
+        NULL,			    // 不在此指定可执行文件的文件名
+        szCommandLine,	    // 命令行参数
+        NULL,			    // 默认进程安全性
+        NULL,			    // 默认线程安全性
+        FALSE,			    // 指定当前进程内的句柄不可以被子进程继承
+        CREATE_NEW_CONSOLE,	// 为新进程创建一个新的控制台窗口
+        NULL,			    // 使用本进程的环境变量
+        NULL,			    // 使用本进程的驱动器和目录
+        &si,
+        &pi
+    );
+
+    if (bRet) {
+        //关闭内核句柄
+        ::CloseHandle(pi.hThread);
+        ::CloseHandle(pi.hProcess);
+        cout << "Process ID " << pi.dwProcessId << endl;
+        cout << "Thread ID " << pi.dwThreadId << endl;
+
+    }
+    system("pause");
+
+    return 0;
+}
+
---
+++ ch1_process/ch1_process_api_learn/show_process.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <TlHelp32.h>
+using namespace std;
+
+int main2() {
+    //保存快照的信息
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof(pe32);
+
+    HANDLE hProcessSnap = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hProcessSnap == INVALID_HANDLE_VALUE) {
+        cout << "Create failure" << endl;
+        return -1;
+
+    }
+    //遍历进程快照信息
+    BOOL bMore = ::Process32First(hProcessSnap, &pe32);
+    while (bMore) {
+        cout << "Process Name: " << pe32.szExeFile << endl;
+        cout << "Process ID: " << pe32.th32ProcessID << endl;
+        bMore = ::Process32Next(hProcessSnap, &pe32);
+    }
+    ::CloseHandle(hProcessSnap);
+    system("pause");
+
+    return 0;
+}
