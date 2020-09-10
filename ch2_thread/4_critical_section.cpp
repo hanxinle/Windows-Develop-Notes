57314e02f62f1e55623a46e1d2d307f569e67ec6
hanxinle <hanxloop@foxmail.com>
2020-09-10 18:43:48 +0800

    thread_complete

---
+++ ch2_thread/4_critical_section.cpp
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+#include <process.h>
+#include <windows.h>
+
+int g_Cnt = 0;
+//全局临界区
+CRITICAL_SECTION g_cs;
+
+
+UINT __stdcall ThreadFunc(LPVOID);
+BOOL bFlag = FALSE;
+
+int main4(void) {
+    UINT uId;
+    HANDLE h[2];
+    
+    ::InitializeCriticalSection(&g_cs);
+
+    h[0] = (HANDLE)::_beginthreadex(NULL, 0, ThreadFunc, NULL, 0, &uId);
+    h[1] = (HANDLE)::_beginthreadex(NULL, 0, ThreadFunc, NULL, 0, &uId);
+    //主线程休息1000ms,让出CPU的使用权，让我们辅助线程有机会运行
+    bFlag = TRUE;
+    Sleep(1000);
+    bFlag = FALSE;
+
+    ::WaitForMultipleObjects(2, h, TRUE, INFINITE);
+    
+    CloseHandle(h[0]);
+    CloseHandle(h[1]);
+
+    ::DeleteCriticalSection(&g_cs);
+
+    printf("g_Cnt=%d\n", g_Cnt);
+    system("pause");
+    return 0;
+}
+//线程的编程的重点：与时间有关的错误
+UINT __stdcall ThreadFunc(LPVOID) {
+    if (bFlag) {
+        //::EnterCriticalSection(&g_cs);
+
+        for (int i = 0; i < 10000; i++)
+            ////g_Cnt++;                          
+            //将 g_Cnt++ 替换为 互锁函数
+            ::InterlockedIncrement((long*)&g_Cnt);  
+
+        //::LeaveCriticalSection(&g_cs);
+    }
+    return 0;
+}
---
+++ ch2_thread/5_price_of_lock.cpp
@@ -0,0 +1,50 @@
+﻿//锁导致程序的推进效率降低，两个值相同但是变小了
+
+#include <stdio.h>
+#include <process.h>
+#include <windows.h>
+
+int g_Cnt_1 = 0;
+int g_Cnt_2 = 0;
+
+//全局临界区
+
+CRITICAL_SECTION g_cs_1;
+
+
+UINT __stdcall ThreadFunc1(LPVOID);
+BOOL bFlag1 = FALSE;
+
+int main5(void) {
+    UINT uId;
+    HANDLE h[2];
+
+    ::InitializeCriticalSection(&g_cs_1);
+
+    h[0] = (HANDLE)::_beginthreadex(NULL, 0, ThreadFunc1, NULL, 0, &uId);
+    h[1] = (HANDLE)::_beginthreadex(NULL, 0, ThreadFunc1, NULL, 0, &uId);
+    //主线程休息1000ms,让出CPU的使用权，让我们辅助线程有机会运行
+    bFlag1 = TRUE;
+    Sleep(1000);
+    bFlag1 = FALSE;
+    ::WaitForMultipleObjects(2, h, TRUE, INFINITE);
+    CloseHandle(h[0]);
+    CloseHandle(h[1]);
+
+    ::DeleteCriticalSection(&g_cs_1);
+    //值应该相同
+    printf("g_Cnt_ = %d,g_Cnt_2 = %d\n", g_Cnt_1,g_Cnt_2);
+    system("pause");
+    return 0;
+}
+
+//线程的编程的重点：与时间有关的错误
+UINT __stdcall ThreadFunc1(LPVOID) {
+    while (bFlag1) {
+        ::EnterCriticalSection(&g_cs_1);
+        g_Cnt_1++;
+        g_Cnt_2++;
+        ::LeaveCriticalSection(&g_cs_1);
+    }
+    return 0;
+}
