2cde214454a86735bbe222814b5f44ebcb7cbb3b
hanxinle <hanxloop@foxmail.com>
2020-09-13 01:55:54 +0800

    multithread

---
+++ ch1_process/ch3_multi_thread/xthread.cpp
@@ -0,0 +1,36 @@
+﻿#include "xthread.h"
+#include <windows.h>
+#include <iostream>
+#include <process.h>
+
+
+
+static void Main(void * para) {
+    xthread * obj = (xthread *)para;
+    obj->usr_define_task();
+    _endthread();
+}
+
+
+bool xthread::start() {
+    r_thread_ =  _beginthread(Main, 0, this);
+    if (r_thread_ <= 0) return false;
+
+    return true;
+}
+
+void xthread::wait() {
+    WaitForSingleObject((HANDLE)r_thread_, INFINITE);
+}
+
+//void xthread::usr_define_task() {
+//
+//}
+
+xthread::xthread() {
+
+}
+
+xthread::~xthread() {
+
+}
