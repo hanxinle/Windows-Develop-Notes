f692fce696c1ff4046af8db68090dd2b36caad88
hanxinle <hanxloop@foxmail.com>
2020-09-15 08:22:24 +0800

    qt_image_deal

---
+++ qt_image_multi_thread/ImageThread.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <QThread>
+
+//每次重新开多个线程
+class ImageThread:public QThread {
+public:
+    static void DealImage(unsigned char * img, unsigned  int size, float bright, int thread_cnt);
+    static int DealPercent();
+    static void Wait();
+    void run();
+
+    unsigned int begin = 0;
+    unsigned int end = 0;
+    unsigned int cur = 0;
+
+    unsigned char * img = NULL;
+    float bright = 1;
+    ImageThread();
+    ~ImageThread();
+};
+
