611948f52344cd56f85f945b5940fbe72a984445
hanxinle <hanxinle199@gmail.com>
2020-12-22 15:42:58 +0800

    del_chatroom

--- ch11_chatroom/src/chatroom/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-﻿// stdafx.cpp : source file that includes just the standard includes
-// ChatRoom.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
