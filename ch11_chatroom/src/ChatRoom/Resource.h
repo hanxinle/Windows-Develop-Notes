5691df15b0cc733858d5cfad34715d3b885bc323
hanxinle <hanxinle199@gmail.com>
2020-12-22 15:43:35 +0800

    add chat room

---
+++ ch11_chatroom/src/ChatRoom/Resource.h
@@ -0,0 +1,54 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ChatRoom.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CHATROOM_DIALOG             102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       132
+#define IDR_MENU2                       133
+#define IDC_NETSET                      1000
+#define IDC_SENDMSG                     1001
+#define IDC_SHOWMSG                     1002
+#define IDC_SHOW_MSG                    1002
+#define IDC_INPUTMSG                    1003
+#define IDC_INPUT_MSG                   1003
+#define IDC_SPEARATE                    1004
+#define IDC_RADIO_CLIENT                1005
+#define IDC_RADIO_SERVER                1006
+#define IDC_IPADDR                      1007
+#define IDC_IP_ADDR                     1007
+#define IDC_CONNECT_PORT                1008
+#define IDC_LISTEN_PORT                 1009
+#define IDC_FRAME                       1010
+#define IDC_START_CLIENT                1011
+#define IDC_START_SERVER                1012
+#define IDC_STATIC_LISTEN_PORT          1013
+#define IDC_STATIC_SERVER_IP            1014
+#define IDC_STATIC_SERVER_PORT          1015
+#define IDC_OTHER                       1016
+#define IDC_STOP_SERVER                 1017
+#define IDC_STOP_CLIENT                 1018
+#define ID_POPUP_32771                  32771
+#define ID_POPUP2_32773                 32773
+#define ID_POPUP2_32774                 32774
+#define ID_MENU_32777                   32777
+#define ID_MENU_TRAYINCO                32778
+#define ID_MENU_32779                   32779
+#define ID_MENU_32780                   32780
+#define ID_MENU_SHOW                    32781
+#define ID_MENU_EXIT                    32782
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ ch11_chatroom/src/ChatRoom/Server.cpp
@@ -0,0 +1,123 @@
+﻿
+#include "stdafx.h"
+#include "ChatRoomDlg.h"
+
+BOOL SOCKET_Select(SOCKET hSocket, int nTimeOut, BOOL bRead) {
+    fd_set fdset;
+    timeval tv;
+    FD_ZERO(&fdset);
+    FD_SET(hSocket, &fdset);
+    nTimeOut = nTimeOut > 1000 ? 1000 : nTimeOut;
+    tv.tv_sec = 0;
+    tv.tv_usec = nTimeOut;
+
+    int iRet = 0;
+    if (bRead) {
+        iRet = select(0, &fdset, NULL, NULL, &tv);
+    }
+    else {
+        iRet = select(0, NULL, &fdset, NULL, &tv);
+    }
+
+    if (iRet <= 0) {
+        return FALSE;
+    }
+    else if (FD_ISSET(hSocket, &fdset)) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+DWORD WINAPI ListenThreadFunc(LPVOID pParam) {
+    CChatRoomDlg *pChatRoom = (CChatRoomDlg *)pParam;
+    ASSERT(pChatRoom != NULL);
+    pChatRoom->m_ListenSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (pChatRoom->m_ListenSock == INVALID_SOCKET) {
+        AfxMessageBox(_T("新建Socket失败！"));
+        return FALSE;
+    }
+
+    int iPort = pChatRoom->GetDlgItemInt(IDC_LISTEN_PORT);
+    if (iPort <= 0 || iPort > 65535) {
+        AfxMessageBox(_T("请输入合适的端口：1 - 65535"));
+        goto __Error_End;
+    }
+
+    sockaddr_in service;
+    service.sin_family = AF_INET;
+    service.sin_addr.s_addr = INADDR_ANY;
+    service.sin_port = htons(iPort);
+
+    if (bind(pChatRoom->m_ListenSock, (sockaddr*)&service, sizeof(sockaddr_in)) == SOCKET_ERROR) {
+        AfxMessageBox(_T("绑定端口失败！"));
+        goto __Error_End;
+    }
+
+    if (listen(pChatRoom->m_ListenSock, 5) == SOCKET_ERROR) {
+        AfxMessageBox(_T("监听失败！"));
+        goto __Error_End;
+    }
+    pChatRoom->ShowMsg(_T("系统信息：启动服务器成功！"));
+    pChatRoom->m_bIsServer = TRUE;
+    pChatRoom->EnableWindow(IDC_START_SERVER, FALSE);
+    pChatRoom->EnableWindow(IDC_STOP_SERVER);
+
+    while (TRUE && !(pChatRoom->bShutDown)) {
+        if (SOCKET_Select(pChatRoom->m_ListenSock, 100, TRUE)) {
+            sockaddr_in clientAddr;
+            int iLen = sizeof(sockaddr_in);
+            SOCKET accSock = accept(pChatRoom->m_ListenSock, (struct sockaddr *)&clientAddr, &iLen);
+            if (accSock == INVALID_SOCKET) {
+                continue;
+            }
+            CClientItem tItem;
+            tItem.m_Socket = accSock;
+            tItem.m_pMainWnd = pChatRoom;
+            tItem.m_strIp = inet_ntoa(clientAddr.sin_addr);
+            INT_PTR idx = pChatRoom->m_ClientArray.Add(tItem);
+            tItem.hThread = CreateThread(NULL, 0, ClientThreadProc, &(pChatRoom->m_ClientArray.GetAt(idx)), CREATE_SUSPENDED, NULL);
+            pChatRoom->m_ClientArray.GetAt(idx).hThread = tItem.hThread;
+            ResumeThread(tItem.hThread);
+            CString strMsg;
+
+            strMsg = _T("客户端：") + tItem.m_strIp + _T(" 进入聊天室！");
+            pChatRoom->ShowMsg(strMsg);
+            pChatRoom->SendClientsMsg(strMsg, &tItem);
+
+            Sleep(100);
+        }
+    }
+
+__Error_End:
+    closesocket(pChatRoom->m_ListenSock);
+    return TRUE;
+}
+
+
+DWORD WINAPI ClientThreadProc(LPVOID lpParameter) {
+    CString strMsg;
+    CClientItem m_ClientItem = *(CClientItem *)lpParameter;
+    while (TRUE && !(m_ClientItem.m_pMainWnd->bShutDown)) {
+        if (SOCKET_Select(m_ClientItem.m_Socket, 100, TRUE)) {
+            TCHAR szBuf[MAX_BUF_SIZE] = { 0 };
+            int iRet = recv(m_ClientItem.m_Socket, (char *)szBuf, MAX_BUF_SIZE, 0);
+            if (iRet > 0) {
+                //right;
+                strMsg = szBuf;
+                strMsg = _T("客户端：") + m_ClientItem.m_strIp + _T(">") + strMsg;
+                m_ClientItem.m_pMainWnd->ShowMsg(strMsg);
+                m_ClientItem.m_pMainWnd->SendClientsMsg(strMsg, &m_ClientItem);
+            }
+            else {
+                //close socket;
+                strMsg = _T("客户端：") + m_ClientItem.m_strIp + _T(" 离开了聊天室！");
+                m_ClientItem.m_pMainWnd->ShowMsg(strMsg);
+                m_ClientItem.m_pMainWnd->RemoveClientFromArray(m_ClientItem);
+                m_ClientItem.m_pMainWnd->SendClientsMsg(strMsg, &m_ClientItem);
+                break;
+            }
+        }
+        Sleep(500);
+    }
+    return TRUE;
+}
